40761583c0860919569ae1726c60178757e3d6dd
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2023-09-02 11:42:01 +0200

    Changed to form for testing purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,42 @@
 import { Button, Input } from "~/components"
 
+async function action(formData: FormData) {
+  "use server"
+
+  console.log(formData)
+}
+
 export default function Home() {
   return (
-    <div className="w-fit space-y-8">
+    <form action={action} className="w-fit space-y-8">
       <Input
         label="Name"
         name="name"
         description="Full name."
-        error="Must be a string."
+        // error="Must be a string."
       />
       <Input
         label="Age"
         name="age"
         type="number"
         description="Don't be underage."
-        error="Must be >= 18."
+        // error="Must be >= 18."
       />
       <Input
         label="Bio"
         name="bio"
         type="textarea"
         description="Tell us more about yourself."
-        error="Must be a string."
+        // error="Must be a string."
       />
       <Input
         label="DateOfBirth"
         name="dateOfBirth"
         type="date"
         description="When were you born?"
-        error="Ain't a date, lad."
+        // error="Ain't a date, lad."
       />
-      <Button>Click me!</Button>
-    </div>
+      <Button type="submit">Click me!</Button>
+    </form>
   )
 }
