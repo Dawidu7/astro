e8fad0c92995628da8ab5ab26eda09f92630bea7
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2023-09-04 18:34:40 +0200

    Footer Text Size Change

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -13,12 +13,12 @@ export default function Footer() {
     !pathname.startsWith("/dashboard") && (
       <footer
         className={clsx(
-          "fixed bottom-0 z-40 w-full bg-zinc-900 py-4 text-center text-4xl font-semibold transition duration-300",
+          "fixed bottom-0 z-40 w-full bg-zinc-900 py-4 text-center font-semibold transition duration-300",
           isVisible && "translate-y-full",
         )}
       >
         <Button
-          className="text-zinc-400 data-[hovered]:text-white"
+          className="text-2xl text-zinc-400 data-[hovered]:text-white md:text-4xl"
           onPress={() => scrollTo({ top: 0, behavior: "smooth" })}
           plain
         >
