8e5ea0c8e9191376e267e0b2914d1b3090e37435
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2023-09-06 05:48:11 +0200

    Dashboard: Data Fetching

--- src/app/(admin)/dashboard/[app]/page.tsx
+++ src/app/(admin)/dashboard/[app]/page.tsx
@@ -1,3 +1,47 @@
+import db from "~/db"
+
 export default async function App({ params }: { params: { app: string } }) {
-  return <div>{params.app}</div>
+  const data = await getData(params.app)
+
+  if (!data) return "No App Found."
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+async function getData(app: string) {
+  switch (app) {
+    case "calculator": {
+      const [cameras, flattReducs, telescopes] = await Promise.all([
+        db.query.cameras.findMany(),
+        db.query.flattReducs.findMany(),
+        db.query.telescopes.findMany(),
+      ])
+
+      return { cameras, flattReducs, telescopes }
+    }
+    case "gallery": {
+      const images = await db.query.images.findMany()
+
+      return { images }
+    }
+    case "generator": {
+      const catalogs = await db.query.catalogs.findMany()
+
+      return { catalogs }
+    }
+    case "planner": {
+      const options = await db.query.options.findMany()
+
+      return options.reduce((acc, option) => {
+        const typeArray = acc[option.type as keyof typeof acc]
+
+        return {
+          ...acc,
+          [option.type]: typeArray ? [...typeArray, option] : [option],
+        }
+      }, {})
+    }
+    default:
+      return null
+  }
 }
