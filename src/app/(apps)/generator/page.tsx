1af7267ad07110ea5fabf1acb344b4d7116d8614
Dawidu7 <dawidtomalik7@gmail.com>
2023-10-02 08:42:05 +0200

    Removed Alphaetical Sorting

--- src/app/(apps)/generator/page.tsx
+++ src/app/(apps)/generator/page.tsx
@@ -2,9 +2,7 @@ import Client from "./Client"
 import db from "~/db"
 
 export default async function Generator() {
-  const catalogs = await db.query.catalogs.findMany({
-    orderBy: (catalogs, { asc }) => [asc(catalogs.name)],
-  })
+  const catalogs = await db.query.catalogs.findMany()
 
   return <Client catalogs={catalogs} />
 }
